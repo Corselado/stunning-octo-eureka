26443043a4f174c7b9b6de6b7b61b5ba1fc458c3
IvanSD <ivansd122015@gmail.com>
2023-11-14 10:47:58 +0100

    Modificado .java para mostrar nombre, instituo. localidad y readme anotado quien

--- App.java
+++ App.java
@@ -1,9 +1,12 @@
 public class App {
   
   public static void main(String[] args) {
-    System.out.println("Contenido 2a");
+    System.out.println("Iván Sánchez Díez");
     System.out.println("Contenido 3");
-    System.out.println("Contenido 2b");
+    System.out.println("IES San Andres");
+    System.out.println("Linea 4");
+    System.out.println("Linea 5");
+    System.out.println("León");
   }
   
 }
